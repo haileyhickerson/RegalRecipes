b8c5d793938f257e7e3b20fc2e268845a4996fbe
hailey <haileyhickerson0@gmail.com>
2024-09-29 19:57:10 -0500

    Initial Files
    Here are the files for my part of Sprint 1! I completed a prototype for the main menu & recipe selection scenes.

---
+++ RegalRecipes/main_menu.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+# Start Button
+func _on_start_button_pressed() -> void:
+	# Proceeds to Recipe Selection Scene
+	get_tree().change_scene_to_file("res://recipe_selection.tscn")
+
+# Quit Button
+func _on_quit_button_pressed() -> void:
+	# Quits Application
+	get_tree().quit()
---
+++ RegalRecipes/recipe_selection.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+# Recipe 1
+func _on_recipe_1_button_pressed() -> void:
+	# Proceeds to Kitchen Scene
+	get_tree().change_scene_to_file("res://kitchen.tscn")
+
+# Recipe 2
+func _on_recipe_2_button_pressed() -> void:
+	# Do nothing for now
+	pass
+
+# Recipe 3
+func _on_recipe_3_button_pressed() -> void:
+	# Do nothing for now
+	pass
+
+# Back Button
+func _on_back_button_pressed() -> void:
+	# Returns to main menu scene
+	get_tree().change_scene_to_file("res://main_menu.tscn")
