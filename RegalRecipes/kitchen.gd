df6f0a13e42cdbecb07ec48033310442c261f20d
hailey <haileyhickerson0@gmail.com>
2024-10-07 05:01:13 -0500

    Added art, a new font, and footstep SFX
    Added chef's hat as placeholder for character, as well as temporary AI concept art for the scene backgrounds. Also added footsteps while moving player & a new font for some of the text.

--- RegalRecipes/kitchen.gd
+++ RegalRecipes/kitchen.gd
@@ -1,30 +1,33 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
+#
 func _on_pantry_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://pantry.tscn") # Replace with function body.
+	# Changes to pantry scene
+	get_tree().change_scene_to_file("res://pantry.tscn") 
 
 func _on_cutting_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://cutting.tscn") # Replace with function body.
+	# Changes to cutting scene
+	get_tree().change_scene_to_file("res://cutting.tscn") 
 
 func _on_stove_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://stove.tscn") # Replace with function body.
-
+	# Changes to stove scene
+	get_tree().change_scene_to_file("res://stove.tscn") 
+	
 func _on_mixing_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://mixing.tscn") # Replace with function body.
+	# Changes to mixing scene
+	get_tree().change_scene_to_file("res://mixing.tscn") 
 
 func _on_plating_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://plating.tscn") # Replace with function body.
+	# Changes to plating scene
+	get_tree().change_scene_to_file("res://plating.tscn") 
 
 func _on_exit_button_pressed() -> void:
 	# Quits Application
--- RegalRecipes/player.gd
+++ RegalRecipes/player.gd
@@ -2,13 +2,13 @@ extends Area2D
 
 @export var speed = 400
 var screen_size
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO 
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -21,17 +21,20 @@ func _process(delta: float) -> void:
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
+		if $FootstepSound.playing == false:
+			$FootstepSound.play()
 	else:
 		$AnimatedSprite2D.stop()
+		$FootstepSound.stop()
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about the following boolean assignment.
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		##$AnimatedSprite2D.flip_v = false
+
+		##$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.animation = "walk"
+		##$AnimatedSprite2D.flip_v = velocity.y > 0
