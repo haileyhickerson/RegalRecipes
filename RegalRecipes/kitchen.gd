745e393c4a7f463a04519f008dc1fa8d31c0b518
lebruce13 <lebruce13@gmail.com>
2024-10-07 14:01:25 -0500

    Removed buttons, added player
    removed the buttons and instead you can move the chef hat to each area and press spacebar so that it can switch scenes

--- RegalRecipes/kitchen.gd
+++ RegalRecipes/kitchen.gd
@@ -1,34 +1,62 @@
 extends Node2D
-
+var in_pantry = false
+var in_cutting = false
+var in_stove = false
+var in_mixing = false
+var in_plating = false
+var in_exit = false
 
 func _ready() -> void:
 	pass 
 
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("action") and in_pantry:
+		get_tree().change_scene_to_file("res://pantry.tscn") 
+	if Input.is_action_pressed("action") and in_cutting:
+		get_tree().change_scene_to_file("res://cutting.tscn") 
+	if Input.is_action_pressed("action") and in_mixing:
+		get_tree().change_scene_to_file("res://mixing.tscn") 
+	if Input.is_action_pressed("action") and in_stove:
+		get_tree().change_scene_to_file("res://stove.tscn") 
+	if Input.is_action_pressed("action") and in_plating:
+		get_tree().change_scene_to_file("res://plating.tscn") 
+	if Input.is_action_pressed("action") and in_exit:
+		get_tree().quit()
 
-#
-func _on_pantry_button_pressed() -> void:
-	# Changes to pantry scene
-	get_tree().change_scene_to_file("res://pantry.tscn") 
-
-func _on_cutting_button_pressed() -> void:
-	# Changes to cutting scene
-	get_tree().change_scene_to_file("res://cutting.tscn") 
-
-func _on_stove_button_pressed() -> void:
-	# Changes to stove scene
-	get_tree().change_scene_to_file("res://stove.tscn") 
-	
-func _on_mixing_button_pressed() -> void:
-	# Changes to mixing scene
-	get_tree().change_scene_to_file("res://mixing.tscn") 
-
-func _on_plating_button_pressed() -> void:
-	# Changes to plating scene
-	get_tree().change_scene_to_file("res://plating.tscn") 
-
-func _on_exit_button_pressed() -> void:
-	# Quits Application
-	get_tree().quit()
+func _on_pantry_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_pantry = true
+func _on_pantry_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_pantry = false
+func _on_cutting_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_cutting = true 
+func _on_cutting_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_cutting = false
+func _on_stove_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_stove = true
+func _on_stove_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_stove = false
+func _on_mixing_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_mixing = true
+func _on_mixing_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_mixing = false
+func _on_plating_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_plating = true
+func _on_plating_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_plating = false
+func _on_exit_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_exit = true
+func _on_exit_area_area_exited(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		in_exit = false
