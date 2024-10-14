b76a9001d46ecb010844d0888b672362f051120c
hailey <haileyhickerson0@gmail.com>
2024-10-14 12:36:57 -0500

    edited dialogue/char art

--- RegalRecipes/player_variables.gd
+++ RegalRecipes/player_variables.gd
@@ -4,7 +4,7 @@ var location
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	location = Vector2(100, 100)
+	location = Vector2(300, 300)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
