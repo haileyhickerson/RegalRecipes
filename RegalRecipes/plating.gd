588021e4971d9e40a365ebfa625478686f675cd5
carolinaolaya553 <olaya.c@yahoo.com>
2024-10-27 22:10:04 -0500

    plating station, added dragging for ingredients

--- RegalRecipes/plating.gd
+++ RegalRecipes/plating.gd
@@ -1,11 +1,16 @@
 extends Node2D
+@onready var ReferencePic = $ReferencePic
+@onready var bowl_sprites=$BowlsContainer
+@onready var Timer1 = $Timer1
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
+func _ready():
+	ReferencePic.visible= true
+	bowl_sprites.visible= false
+	Timer1.wait_time= 3.0
+	Timer1.start()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -15,4 +20,10 @@ func _on_back_button_pressed() -> void:
 
 func _on_taste_test_button_pressed()-> void:
 	get_tree().change_scene_to_file("res://taste_test.tscn")
+	
+
+
+func _on_timer_1_timeout() -> void:
+	ReferencePic.visible= false
+	bowl_sprites.visible= true
 	
---
+++ RegalRecipes/plating_ingredients.gd
@@ -0,0 +1,39 @@
+extends Area2D
+var is_dragging = false
+var mouse_offset = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$CollisionShape2D.disabled = false
+	set_process_input(true)
+	self.connect("input_event", Callable(self, "_on_input_event"))
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			# Check if the mouse click is inside the CollisionShape2D
+			is_dragging = true
+			mouse_offset = global_position- event.position
+		else:
+			is_dragging = false
+
+func _process(delta):
+	# If dragging, follow the mouse
+	if is_dragging:
+		global_position= get_global_mouse_position() +mouse_offset
+
+
+#func _on_Area2D_input_event(viewport,event, shape_idx):
+	#if event is InputEventMouseMotion:
+		#is_mouse_over= true
+	#elif event is InputEventMouseButton:
+		#if not event.presed:
+			#is_mouse_over= false
+
+#func check_drop():
+	## Check if the ingredient is dropped on a valid spot
+	#var plate = get_parent().get_parent().get_parent()  # Adjust to get the Plate node
+	#for spot in plate.get_children():
+		#if spot.is_in_group("plate_spots") and spot.get_global_transform().origin.has_point(get_global_mouse_position()):
+			#position = spot.position  # Snap the area to the spot
+			#print("Dropped on " + spot.name)
+			#return
