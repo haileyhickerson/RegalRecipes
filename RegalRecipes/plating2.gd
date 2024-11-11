c28c230e9c1e2b6c203a14c64e8e7f1f9c61e473
carolinaolaya553 <olaya.c@yahoo.com>
2024-11-11 13:27:17 -0600

    plating for recipe 2

--- RegalRecipes/plating2.gd
+++ RegalRecipes/plating2.gd
@@ -26,13 +26,13 @@ var carrot3_correct = false;
 
 var mouse_offset = Vector3.ZERO;
 
-var score=0
+var score2=0
 var first_space_pressed = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ReferencePic.visible= true
 	$HintReferencePic.visible=false
-	bowl_sprites.visible= false
+	$PlateContainer.visible= false
 	$HintButton.hide()
 	#Timer1.wait_time= 3.0
 	#Timer1.start()
@@ -41,6 +41,7 @@ func _ready():
 	$ButterspoonHead.visible= false
 	$HintText.visible=false
 	$ContinueButton.hide()
+	$"Taste Test Button".hide()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -69,23 +70,23 @@ func _process(delta: float) -> void:
 	
 
 func update_score_label():
-	if score<0:
-		score=0
-	ScoringLabel.text= "Final Score: "+ str(score)
+	if score2<0:
+		score2=0
+	ScoringLabel.text= "Final Score: "+ str(score2)
 	
-func add_score(points):
-	score+= points
+func add_score(points2):
+	score2+= points2
 
-func subtract_score(points):
-	score -= points
+func subtract_score(points2):
+	score2 -= points2
 
 
 func _on_taste_test_button_pressed()-> void:
 	$"Taste Test Button".hide()
 	update_score_label()
-	PlayerVariables.plating_score= score
+	PlayerVariables.plating_score= score2
 	ScoreTextBox.visible= true
-	print("Plating complete! Final Score: ", score)
+	print("Plating complete! Final Score: ", score2)
 	PlayerVariables.plating_completed=true
 	$ContinueButton.show()
 
@@ -93,7 +94,8 @@ func _on_taste_test_button_pressed()-> void:
 
 func _on_timer_1_timeout() -> void:
 	ReferencePic.visible= false
-	bowl_sprites.visible= true
+	$"Taste Test Button".show()
+	$PlateContainer.visible= true
 	$FancyCircle.visible=true
 	$ButterspoonHead.visible= true
 	$HintText.visible=true
@@ -101,171 +103,171 @@ func _on_timer_1_timeout() -> void:
 
 
 # check if peas are currently being pressed down
-func _on_pea_button_button_down() -> void:
-	peas_dragging = true;
-	mouse_offset = $BowlsContainer/BlueBowl/Peas.global_position - get_global_mouse_position()
+func _on_steak_button_down() -> void:
+	steak_dragging = true;
+	mouse_offset = $PlateContainer/BluePlate/Steak.global_position - get_global_mouse_position()
 
 # check if peas have been released
-func _on_pea_button_button_up() -> void:
-	peas_dragging = false;
+func _on_steak_button_up() -> void:
+	steak_dragging = false;
 
 
-# check if cheese 1 is currently being pressed down
-func _on_cheese_1_button_button_down() -> void:
-	cheese1_dragging = true;
-	mouse_offset = $BowlsContainer/PinkBowl/Cheese1.global_position - get_global_mouse_position()
+# check if potato is currently being pressed down
+func _on_potato_button_down() -> void:
+	potato_dragging = true;
+	mouse_offset = $PlateContainer/PurplePlate/Potato.global_position - get_global_mouse_position()
 
-# check if cheese 1 has been released
-func _on_cheese_1_button_button_up() -> void:
-	cheese1_dragging = false;
+# check if potato has been released
+func _on_potato_button_up() -> void:
+	potato_dragging = false;
 
-# check if cheese 2 is currently being pressed down
-func _on_cheese_2_button_button_down() -> void:
-	cheese2_dragging = true;
-	mouse_offset = $BowlsContainer/PinkBowl/Cheese2.global_position - get_global_mouse_position()
+# check if butter1 is currently being pressed down
+func _on_butter_1_button_down() -> void:
+	butter1_dragging = true;
+	mouse_offset = $PlateContainer/GreenPlate/Butter1.global_position - get_global_mouse_position()
 
-# check if cheese 2 has been released
-func _on_cheese_2_button_button_up() -> void:
-	cheese2_dragging = false;
+# check if butter1 has been released
+func _on_butter_1_button_up() -> void:
+	butter1_dragging = false;
 
-# check if cheese 3 has been released
-func _on_cheese_3_button_button_down() -> void:
-	cheese3_dragging = true;
-	mouse_offset = $BowlsContainer/PinkBowl/Cheese3.global_position - get_global_mouse_position()
+# check if butter2 has been released
+func _on_butter_2_button_down() -> void:
+	butter2_dragging = true;
+	mouse_offset = $PlateContainer/GreenPlate/Butter2.global_position - get_global_mouse_position()
 
-# check if cheese 3 has been released
-func _on_cheese_3_button_button_up() -> void:
-	cheese3_dragging = false;
+# check if butter2 has been released
+func _on_butter_2_button_up() -> void:
+	butter2_dragging = false;
 	
-func _on_bacon_1_button_button_down() -> void:
-	bacon1_dragging = true;
-	mouse_offset = $BowlsContainer/YellowPlate/Bacon1.global_position - get_global_mouse_position()
+func _on_carrot_1_button_down() -> void:
+	carrot1_dragging = true;
+	mouse_offset = $PlateContainer/PinkPlate/Carrot1.global_position - get_global_mouse_position()
 
 
-func _on_bacon_1_button_button_up() -> void:
-	bacon1_dragging = false;
+func _on_carrot_1_button_up() -> void:
+	carrot1_dragging = false;
 
 
-func _on_bacon_2_button_button_down() -> void:
-	bacon2_dragging = true;
-	mouse_offset = $BowlsContainer/YellowPlate/Bacon2.global_position - get_global_mouse_position()
+func _on_carrot_2_button_down() -> void:
+	carrot2_dragging = true;
+	mouse_offset = $PlateContainer/PinkPlate/Carrot2.global_position - get_global_mouse_position()
 
 
-func _on_bacon_2_button_button_up() -> void:
-	bacon2_dragging = false;
+func _on_carrot_2_button_up() -> void:
+	carrot2_dragging = false;
 
-func _on_bacon_3_button_button_down() -> void:
-	bacon3_dragging = true;
-	mouse_offset = $BowlsContainer/YellowPlate/Bacon3.global_position - get_global_mouse_position()
+func _on_carrot_3_button_down() -> void:
+	carrot3_dragging = true;
+	mouse_offset = $PlateContainer/PinkPlate/Carrot3.global_position - get_global_mouse_position()
 
 
-func _on_bacon_3_button_button_up() -> void:
-	bacon3_dragging = false;
+func _on_carrot_3_button_up() -> void:
+	carrot3_dragging = false;
 	
 
 
 # checks when center of peas has entered pea target area
-func _on_pea_target_area_entered(area: Area2D) -> void:
-	if area.is_in_group("peas"):
-		print("peas are in correct location!")
-		peas_correct = true;
+func _on_steak_target_area_entered(area: Area2D) -> void:
+	if area.is_in_group("steak"):
+		print("steak are in correct location!")
+		steak_correct = true;
 		add_score(10)
 		
 
 # checks when center of peas has exited pea target area
-func _on_pea_target_area_exited(area: Area2D) -> void:
-	if area.is_in_group("peas"):
-		print("peas are no longer in correct location!")
-		peas_correct = false;
+func _on_steak_target_area_exited(area: Area2D) -> void:
+	if area.is_in_group("steak"):
+		print("steak are no longer in correct location!")
+		steak_correct = false;
 		subtract_score(10)
 
 # checks when center of a cheese has entered cheese target area 1
-func _on_cheese_target_1_area_entered(area: Area2D) -> void:
-	if area.is_in_group("cheese"):
-		print("cheese 1 is in correct location!")
-		cheese1_correct = true;
+func _on_potato_target_area_entered(area: Area2D) -> void:
+	if area.is_in_group("potato"):
+		print("potato is in correct location!")
+		potato_correct = true;
 		add_score(15)
 
 # checks when center of a cheese has exited cheese target area 1
-func _on_cheese_target_1_area_exited(area: Area2D) -> void:
-	if area.is_in_group("cheese"):
-		print("cheese 1 is no longer in correct location!")
-		cheese1_correct = false;
+func _on_potato_target_area_exited(area: Area2D) -> void:
+	if area.is_in_group("potato"):
+		print("potato is no longer in correct location!")
+		potato_correct = false;
 		subtract_score(15)
 
 
 # checks when center of a cheese has entered cheese target area 2
-func _on_cheese_target_2_area_entered(area: Area2D) -> void:
-	if area.is_in_group("cheese"):
-		print("cheese 2 is in correct location!")
-		cheese2_correct = true;
+func _on_butter_target_1_area_entered(area: Area2D) -> void:
+	if area.is_in_group("butter"):
+		print("butter 1 is in correct location!")
+		butter1_correct = true;
 		add_score(15)
 
 # checks when center of a cheese has exited cheese target area 2
-func _on_cheese_target_2_area_exited(area: Area2D) -> void:
-	if area.is_in_group("cheese"):
-		print("cheese 2 is no longer in correct location!")
-		cheese2_correct = false;
+func _on_butter_target_1_area_exited(area: Area2D) -> void:
+	if area.is_in_group("butter"):
+		print("butter 1 is no longer in correct location!")
+		butter1_correct = false;
 		subtract_score(15)
 
 		
 # checks when center of a cheese has entered cheese target area 3
-func _on_cheese_target_3_area_entered(area: Area2D) -> void:
-	if area.is_in_group("cheese"):
-		print("cheese 3 is in correct location!")
-		cheese3_correct = true;
+func _on_butter_target_2_area_entered(area: Area2D) -> void:
+	if area.is_in_group("butter"):
+		print("butter 2 is in correct location!")
+		butter2_correct = true;
 		add_score(15)
 
 # checks when center of a cheese has exited cheese target area 3
-func _on_cheese_target_3_area_exited(area: Area2D) -> void:
-	if area.is_in_group("cheese"):
-		print("cheese 3 is no longer in correct location!")
-		cheese3_correct = false;
+func _on_butter_target_2_area_exited(area: Area2D) -> void:
+	if area.is_in_group("butter"):
+		print("butter 2 is no longer in correct location!")
+		butter2_correct = false;
 		subtract_score(15)
 
 
 # checks when center of a piece of bacon has entered bacon target area 1
-func _on_bacon_target_1_area_entered(area: Area2D) -> void:
-	if area.is_in_group("bacon"):
-		print("bacon 1 is in correct location!")
-		bacon1_correct = true;
+func _on_carrot_target_1_area_entered(area: Area2D) -> void:
+	if area.is_in_group("carrot"):
+		print("carrot 1 is in correct location!")
+		carrot1_correct = true;
 		add_score(15)
 
 # checks when center of a piece of bacon has exited bacon target area 1
-func _on_bacon_target_1_area_exited(area: Area2D) -> void:
-	if area.is_in_group("bacon"):
-		print("bacon 1 is no longer in correct location!")
-		bacon1_correct = false;
+func _on_carrot_target_1_area_exited(area: Area2D) -> void:
+	if area.is_in_group("carrot"):
+		print("carrot 1 is no longer in correct location!")
+		carrot1_correct = false;
 		subtract_score(15)
 
 
 # checks when center of a piece of bacon has entered bacon target area 2
-func _on_bacon_target_2_area_entered(area: Area2D) -> void:
-	if area.is_in_group("bacon"):
+func _on_carrot_target_2_area_entered(area: Area2D) -> void:
+	if area.is_in_group("carrot"):
 		print("bacon 2 is in correct location!")
-		bacon2_correct = true;
+		carrot2_correct = true;
 		add_score(15)
 
 # checks when center of a piece of bacon has exited bacon target area 2
-func _on_bacon_target_2_area_exited(area: Area2D) -> void:
-	if area.is_in_group("bacon"):
-		print("bacon 2 is no longer in correct location!")
-		bacon2_correct = false;
+func _on_carrot_target_2_area_exited(area: Area2D) -> void:
+	if area.is_in_group("carrot"):
+		print("carrot 2 is no longer in correct location!")
+		carrot2_correct = false;
 		subtract_score(15)
 
 
 # checks when center of a piece of bacon has entered bacon target area 3
-func _on_bacon_target_3_area_entered(area: Area2D) -> void:
-	if area.is_in_group("bacon"):
-		print("bacon 3 is in correct location!")
-		bacon3_correct = true;
+func _on_carrot_target_3_area_entered(area: Area2D) -> void:
+	if area.is_in_group("carrot"):
+		print("carrot 3 is in correct location!")
+		carrot3_correct = true;
 		add_score(15)
 
 # checks when center of a piece of bacon has exited bacon target area 3
-func _on_bacon_target_3_area_exited(area: Area2D) -> void:
-	if area.is_in_group("bacon"):
-		print("bacon 3 is no longer in correct location!")
-		bacon3_correct = false;
+func _on_carrot_target_3_area_exited(area: Area2D) -> void:
+	if area.is_in_group("carrot"):
+		print("carrot 3 is no longer in correct location!")
+		carrot3_correct = false;
 		subtract_score(15)
 
 func _on_continue_button_pressed() -> void:
