02760ccbd65040be59fde48fa33ab43e2eeeec41
carolinaolaya553 <olaya.c@yahoo.com>
2024-11-30 00:13:13 -0600

    plating 3

--- RegalRecipes/cake_layers.gd
+++ RegalRecipes/cake_layers.gd
@@ -1,9 +1,18 @@
 extends Node2D
-var first_space_pressed
+var first_space_pressed = false
+var first_layer = false
+var second_layer = false
+var third_layer = false
+var fourth_layer = false
+
+
+var station_completed = false
 var plate_center=960
 var total_score=0
 func _ready():
 	# set the direction and speed to each layer
+	var first_space_pressed = false
+	var second_space_pressed = false
 	$SpongeLayer1.set_direction_and_speed(1, 380)
 	$RasberryFilling.set_direction_and_speed(-1, 450)
 	$SpongeLayer2.set_direction_and_speed(1, 300)
@@ -12,38 +21,54 @@ func _ready():
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("action"):
 		if !first_space_pressed:
-			first_space_pressed = true
 			$TextBox.visible= false
-
+			print("debug1")
+			first_space_pressed = true
+		if station_completed == true:
+			get_tree().change_scene_to_file("res://plating3.tscn")
 
 func _on_layer_1_button_up() -> void:
-	stack_layer($SpongeLayer1)
-	var layer1_score = calculate_score($SpongeLayer1)
-	total_score += layer1_score
-	print("Layer 1 score: ", layer1_score)
-	update_score_display()
+	if !first_layer and !second_layer and !third_layer and !fourth_layer:
+		stack_layer($SpongeLayer1)
+		var layer1_score = calculate_score($SpongeLayer1)
+		total_score += layer1_score
+		print("Layer 1 score: ", layer1_score)
+		update_score_display()
+		first_layer = true
 
 func _on_layer_2_button_up() -> void:
-	stack_layer($RasberryFilling)
-	var layer2_score=calculate_score($RasberryFilling)
-	total_score += layer2_score
-	print("Layer 2 score: ", layer2_score)
-	update_score_display()
+	if first_layer and !second_layer and !third_layer and !fourth_layer:
+		stack_layer($RasberryFilling)
+		var layer2_score=calculate_score($RasberryFilling)
+		total_score += layer2_score
+		print("Layer 2 score: ", layer2_score)
+		update_score_display()
+		second_layer= true
 		
 func _on_layer_3_button_up() -> void:
-	stack_layer($SpongeLayer2)
-	var layer3_score= calculate_score($SpongeLayer2)
-	total_score += layer3_score
-	print("Layer 3 score: ", layer3_score)
-	update_score_display()
+	if first_layer and second_layer and !third_layer and !fourth_layer:
+		stack_layer($SpongeLayer2)
+		var layer3_score= calculate_score($SpongeLayer2)
+		total_score += layer3_score
+		print("Layer 3 score: ", layer3_score)
+		update_score_display()
+		third_layer= true
 		
 func _on_layer_4_button_up() -> void:
-	stack_layer($Frosting)
-	var layer4_score= calculate_score($Frosting)
-	total_score += layer4_score
-	print("Layer 4 score: ", layer4_score)
-	print(total_score)
-	update_score_display()
+	if first_layer and second_layer and third_layer and !fourth_layer:
+		stack_layer($Frosting)
+		var layer4_score= calculate_score($Frosting)
+		total_score += layer4_score
+		print("Layer 4 score: ", layer4_score)
+		print(total_score)
+		update_score_display()
+		$ScoreTextBox/Scoring.text = "Final Score: " + str(total_score)
+		$ScoreTextBox.visible = true
+		$Frosting/Layer4.release_focus()
+		fourth_layer=true
+		station_completed = true
+
+		
 	
 func calculate_score(layer):
 	var alignment_threshold = 60  
